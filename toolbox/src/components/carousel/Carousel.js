7ba35fc9175b5741051b5a28399a56a3def4a1d3
jovan <krsticjovan@protonmail.com>
2024-01-13 22:53:10 +0100

    carousel - added left and right buttons
    partially responsive, fully working animations

--- toolbox/src/components/carousel/Carousel.js
+++ toolbox/src/components/carousel/Carousel.js
@@ -1,61 +1,79 @@
 import React, { Component, useEffect } from "react";
 import "./Carousel.css"
 
-function Carousel({carouselItems = [], autoslide = false}) {
+function Carousel({carouselItems = []}) {
         
     useEffect(() => {
       let currentIndex = 0
-      function handleKeyPress(event) {
-        // console.log(event)
-        if (event.key === 'ArrowLeft') {
-          if (currentIndex == 0)
+      let firstSlide = carouselItems[0];
+      console.log(firstSlide)
+      function carouselCloning() {
+        carouselItems.append(carouselItems[0])
+      }
+      function moveLeft () {
+        if (currentIndex == 0)
             return
           currentIndex--
           
-          console.log("stiso si levo")
-          let drugi = document.querySelectorAll('.carousel-slide-div')[1]
+          let currentSlide = document.querySelectorAll('.carousel-slide-div')[1]
           let carousel = document.querySelector('.carousel-sliding-div')
 
-          // carousel.scrollTo(currentIndex * drugi.clientWidth , 0)
-          carousel.style.transform = `translateX(-${currentIndex * drugi.clientWidth}px)` 
-        }
-        else if (event.key === 'ArrowRight'){
-          if (currentIndex == carouselItems.length - 1)
+          carousel.style.transform = `translateX(-${currentIndex * currentSlide.clientWidth}px)` 
+      
+
+      }
+
+      function moveRight () {
+        if (currentIndex == carouselItems.length - 1)
             return
           currentIndex++
-          console.log(currentIndex)
-          let drugi = document.querySelectorAll('.carousel-slide-div')[1]
+          let currentSlide = document.querySelectorAll('.carousel-slide-div')[1]
           let carousel = document.querySelector('.carousel-sliding-div')
 
-          carousel.style.transform = `translateX(-${currentIndex * drugi.clientWidth}px)`
-          // carousel.scrollTo(currentIndex * drugi.clientWidth , 0)
-        }
-        
-       
-        else if (event.key === 'ArrowUp') 
-          console.log("stiso si gore")
-        
-        else 
-        {console.log("stiso si Dole")}
-
+          carousel.style.transform = `translateX(-${currentIndex * currentSlide.clientWidth}px)`
         
       }
+      function handleKeyPress(event) {
+
+        if (event.key === 'ArrowLeft') {
+          moveLeft()
+            }
+
+        else if (event.key === 'ArrowRight'){
+          moveRight()
+        }
+        else {
+          console.log("error catch")
+        }
+      }
+      
+        document.querySelectorAll(".button-left") [0].addEventListener("click", moveLeft)
+        document.querySelectorAll(".button-right")[0].addEventListener("click", moveRight)
         window.addEventListener("keydown", handleKeyPress);
         return () => {
           window.removeEventListener('keydown', handleKeyPress);
+          
+      document.querySelector(".button-left").removeEventListener("click", moveLeft);
+      document.querySelector(".button-right").removeEventListener("click", moveRight);
+      
+
+
         };
       }, []);
+  
+      
 
-    
+
+      
     const InitialisingItems = () => {
-
+        carouselItems.push(carouselItems[0])
         return carouselItems.map(item => {
           
             return (
               <div className="carousel-slide-div" key={item.Id}>
+                
                 <img className="carousel-image" src={item.imageSource} alt={item.altImage}   />
-                <p className="imageTitle" > {item.imageTitle} </p>
-                <p className="carousel-img-paragraph"> {item.imageParagraph} </p>
+            
               </div>
             )
         })
@@ -67,8 +85,14 @@ function Carousel({carouselItems = [], autoslide = false}) {
     return ( 
         
         <div className="carousel-main-div">
-
+                  <button className="button-left"  >
+                    <img  className="button-png" src="/resources/arrow-left.png" />
+                  </button>
+                  <button className="button-right"  >
+                    <img className="button-png" src="/resources/arrow-right.png" />
+                  </button>
           <div className="carousel-sliding-div">
+            
             {InitialisingItems()}
 
           </div>
@@ -76,7 +100,6 @@ function Carousel({carouselItems = [], autoslide = false}) {
         </div>
 
      );
-
 }
 
 export default Carousel;
