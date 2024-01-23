341bf8d39c2b48e28552049405ad629cc3a4b916
jovan <krsticjovan@protonmail.com>
2024-01-23 19:51:25 +0100

    transparentHeader/dropdownMenu  V1.1

--- toolbox/src/components/TransparentHeader/TransparentHeader.js
+++ toolbox/src/components/TransparentHeader/TransparentHeader.js
@@ -1,37 +1,42 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import "./TransparentHeader.css"
 import { hover } from "@testing-library/user-event/dist/hover";
 import { getSpaceUntilMaxLength } from "@testing-library/user-event/dist/utils";
 import userEvent from "@testing-library/user-event";
 import gsap from "gsap";
+import { unmountComponentAtNode } from "react-dom";
 function TransparentHeader() {
     const dropdownRef = useRef(null)
-    const [hover, setHover] = useState(false)
-    useEffect(() => {
-        if (hover){
-            gsap.to(dropdownRef.current, {height: '0', duration:0.3, ease: "power1.inOut"})
+    const [toggle, setToggle] = useState(false)
+    let availWidth = window.screen.availWidth;
+    
+        
+    function handleMouseClick(){
+        setToggle(!toggle)
+    }
+    useLayoutEffect(()=> {
+        if (toggle){
+            gsap.to(dropdownRef.current, {height: 'auto', duration:0.3, ease: "power1.Out"})
         }
         else{
-            gsap.from(dropdownRef.current, {height: 'auto', duration:0.3, ease: "power1.inOut"})
+            gsap.to(dropdownRef.current, {height: '0', duration:0.3, ease: "power1.in"})
+            console.log("usracu ti se u zivot kurvo", window.screen.availWidth)
         }
     })
-    function handleHoverDropdown(){
+
+    
+
+    function handleMouseEnter(){
         console.log(" u hovered");
-        setHover(true);
+        setToggle(true);
     }
     
-    function handleHoverLeave(){
+    function handleMouseLeave(){
         console.log("u left")
-        setHover(false);
+        setToggle(false);
     }
 
-    function DrawDropDown() {
-        if (hover == true) {
-            
-        
-        }
-        
-    }
+
 
 
 
@@ -42,7 +47,7 @@ function TransparentHeader() {
                 <a href="/">Home</a>
                 <a href="/about">About</a>
                 <a href="/contact">Contact</a>
-                <div onMouseEnter={handleHoverDropdown} onMouseLeave={handleHoverLeave} className="services">Services</div>
+                <div onMouseEnter={handleMouseEnter} onClick={handleMouseClick} onMouseLeave={handleMouseLeave} className="services">Services</div>
             </div>
             <div ref={dropdownRef} className="dropdown-menu">
                     <a className="service-button" href="/random">Service 1</a>
@@ -52,7 +57,6 @@ function TransparentHeader() {
                     <a className="service-button" href="/random">Service 5</a>
                     <a className="service-button" href="/random">Service 6</a>
             </div>
-            {DrawDropDown()}
         </div>
     );
 }
