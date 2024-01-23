0a86803e37f1fdd6e4bda097d56d2d59a64116e8
jovan <krsticjovan@protonmail.com>
2024-01-23 19:53:09 +0100

    transparentHeader/hamburgerButton v1.0
    made responsive (dissapears when screen width goes over the set value. using useState)

--- toolbox/src/components/TransparentHeader/TransparentHeader.js
+++ toolbox/src/components/TransparentHeader/TransparentHeader.js
@@ -7,11 +7,32 @@ import gsap from "gsap";
 import { unmountComponentAtNode } from "react-dom";
 function TransparentHeader() {
     const dropdownRef = useRef(null)
-    const [toggle, setToggle] = useState(false)
-    let availWidth = window.screen.availWidth;
+    const hamburgerMenuRef = useRef(null)
+    const hamburgerButton  = useRef(null)
+    let widthRef = useRef(window.screen.availWidth)
+    let widthRefCurrent = widthRef.current
     
-        
+    const [toggle, setToggle] = useState(false)
+    const [hamburger, setHamburger] = useState(false)
+    
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    const updateScreenWidth = () => {
+        setScreenWidth(window.innerWidth);
+    };
+    useEffect(() => {
+        window.addEventListener('resize', updateScreenWidth);
+
+        return () => {
+            window.removeEventListener('resize', updateScreenWidth);
+        };
+    }, []); 
+
+
+
+
     function handleMouseClick(){
+        
         setToggle(!toggle)
     }
     useLayoutEffect(()=> {
@@ -20,10 +41,27 @@ function TransparentHeader() {
         }
         else{
             gsap.to(dropdownRef.current, {height: '0', duration:0.3, ease: "power1.in"})
-            console.log("usracu ti se u zivot kurvo", window.screen.availWidth)
         }
+       
+        
     })
 
+    
+    useEffect(() => {
+
+
+        if (screenWidth < 1500) 
+        {
+             console.log(screenWidth, "ovo je screen width")
+            hamburgerButton.current.style.width = "30px";
+
+            // hamburgerMenuRef.current.style.width = "0";
+            // hamburgerButton.current.style.display = "none"
+        }
+        else {
+            hamburgerButton.current.style.width = "0px"
+        }
+    },[screenWidth])
     
 
     function handleMouseEnter(){
@@ -41,8 +79,14 @@ function TransparentHeader() {
 
 
     return ( 
-        <div   className="header-main-div" >
-            <img src="/resources/arrow-left.png" alt="logo" />
+        <div  className="header-main-div" >
+            <img className="image" src="/resources/arrow-left.png" alt="logo" />
+            <div ref={hamburgerMenuRef} className="hamburger-menu-div" >
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+                <div onMouseEnter={handleMouseEnter} onClick={handleMouseClick} onMouseLeave={handleMouseLeave} className="services">Services</div>
+            </div>
             <div className="menu-div" >
                 <a href="/">Home</a>
                 <a href="/about">About</a>
@@ -57,9 +101,21 @@ function TransparentHeader() {
                     <a className="service-button" href="/random">Service 5</a>
                     <a className="service-button" href="/random">Service 6</a>
             </div>
+            <img  ref={hamburgerButton} src="/resources/arrow-left.png" className="hamburger-svg" alt="menu" ></img>
         </div>
     );
 }
 
 
 export default TransparentHeader;
+
+
+
+
+// if(availableWidth > 700){
+//     gsap.to(hamburgerMenuRef.current, {width: 'auto', duration:0.3, ease: "power1.Out"})
+// }
+// else{
+//     gsap.to(hamburgerMenuRef.current, {width: '0', duration:0.3, ease: "power1.in"})
+
+// }
