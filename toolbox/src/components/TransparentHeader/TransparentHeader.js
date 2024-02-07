7388eea6ce6426aba0c846e38d02c1710788d2d1
kjovandev <krsticjovan@protonmail.com>
2024-02-07 15:22:55 -0500

    new laptop

--- toolbox/src/components/TransparentHeader/TransparentHeader.js
+++ toolbox/src/components/TransparentHeader/TransparentHeader.js
@@ -10,7 +10,7 @@ function TransparentHeader() {
     const menuRef = useRef(null)
     const hamburgerBackButtonRef = useRef(null)
     const dropdownButtonHover = useHover(dropdownButtonRef);
-    const dropdownHover = useHover(dropdownRef);    
+    const dropdownHover = useHover(dropdownRef);
     const [toggle, setToggle] = useState(false);
     const [hamburger, setHamburger] = useState(false);
     const [screenWidth, setScreenWidth] = useState(window.innerWidth);
@@ -19,35 +19,42 @@ function TransparentHeader() {
         setScreenWidth(window.innerWidth);
     };
     useEffect(() => {
-        if (dropdownHover == false && dropdownButtonHover == false)
-        {
-            setToggle(false)
-            console.log("toggle je false")
-        }
-        else{
+        // console.log("butref is: ", dropdownButtonHover)
+        // console.log("dropref is ", dropdownHover)
+        let timeoutHandler
+        if (dropdownHover || dropdownButtonHover) {
             console.log("toggle je true")
-            console.log("butref is: ",dropdownButtonHover)
-            console.log("dropref is ", dropdownHover)
             setToggle(true)
         }
+        else {
+            console.log("set timeout");
+            timeoutHandler = setTimeout(() => {
+                console.log("dropref is ", dropdownHover)
+                setToggle(false)
+                console.log("button je ", dropdownButtonHover)
+           }, 500);
+        }
 
-
-    },[dropdownButtonHover, dropdownHover]);
+        return () => {
+            console.log("cleared timeout");
+            // clearTimeout(timeoutHandler)
+        };
+    }, [dropdownHover, dropdownButtonHover, toggle]);
 
     useEffect(() => {
         window.addEventListener("resize", updateScreenWidth);
 
         return () => {
-        window.removeEventListener("resize", updateScreenWidth);
+            window.removeEventListener("resize", updateScreenWidth);
         };
     }, []);
 
 
 
-    function handleHamburgerClick(){
+    function handleHamburgerClick() {
         setHamburger(!hamburger)
     }
-    
+
 
 
 
@@ -57,10 +64,9 @@ function TransparentHeader() {
         if (toggle == true && screenWidth < 600) {
             dropdownRef.current.style.transform = 'translateX(-200%)'
         }
-        
-        else if (toggle == true && screenWidth >= 600)
-        {
-            dropdownRef.current.style.transform = 'translateY(+200%)'
+
+        else if (toggle == true && screenWidth >= 600) {
+            dropdownRef.current.style.transform = 'translateY(100%)'
         }
         else {
             dropdownRef.current.style.transform = ''
@@ -68,8 +74,8 @@ function TransparentHeader() {
         if (hamburger) {
             menuRef.current.style.transform = 'translateX(-100%)'
             setTimeout(() => {
-                console.log("buthov je",dropdownButtonHover[1])
-            console.log('drophov je', dropdownHover[1])
+                console.log("buthov je", dropdownButtonHover[1])
+                console.log('drophov je', dropdownHover[1])
             }, 3000);
         }
         else {
@@ -90,44 +96,44 @@ function TransparentHeader() {
 
     return (
         <div className="header-main-div">
-        <img className="logo-header" src="/resources/arrow-left.png" alt="logo" />
-        <div  ref={menuRef} className="menu-div">
-            <div  ref={hamburgerBackButtonRef} className="back-button-hamburger-menu menu-item" onClick={handleHamburgerClick} >Back</div>
-            <div className="menu-item" href="/"><p>Home</p></div>
-            <div className="menu-item" href="/about"><p>About</p></div>
-            <div className="menu-item" href="/contact"><p>Contact</p></div>
-            <div ref={dropdownButtonRef} className="services menu-item">
-            <p>services</p>
-            <div ref={dropdownRef} className="dropdown-menu">
-            <div className="service-button first-service-button" href="/random">
-            Service 1
+            <img className="logo-header" src="/resources/arrow-left.png" alt="logo" />
+            <div ref={menuRef} className="menu-div">
+                <div ref={hamburgerBackButtonRef} className="back-button-hamburger-menu menu-item" onClick={handleHamburgerClick} >Back</div>
+                <div className="menu-item" href="/"><p>Home</p></div>
+                <div className="menu-item" href="/about"><p>About</p></div>
+                <div className="menu-item" href="/contact"><p>Contact</p></div>
+                <div ref={dropdownButtonRef} className="services menu-item">
+                    <p>services</p>
+                    <div ref={dropdownRef} className="dropdown-menu">
+                        <div className="service-button first-service-button" href="/random">
+                            Service 1
+                        </div>
+                        <div className="service-button" href="/random">
+                            Service 2
+                        </div>
+                        <div className="service-button" href="/random">
+                            Service 3
+                        </div>
+                        <div className="service-button" href="/random">
+                            Service 4
+                        </div>
+                        <div className="service-button" href="/random">
+                            Service 5
+                        </div>
+                        <div className="service-button" href="/random">
+                            Service 6
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="service-button" href="/random">
-            Service 2
-            </div>
-            <div className="service-button" href="/random">
-            Service 3
-            </div>
-            <div className="service-button" href="/random">
-            Service 4
-            </div>
-            <div className="service-button" href="/random">
-            Service 5
-            </div>
-            <div className="service-button" href="/random">
-            Service 6
-            </div>
-        </div>
-            </div>
-        </div>
-       
-        <img
-            ref={hamburgerButton}
-            onClick={handleHamburgerClick}
-            src="/resources/hamburgerGray.png"
-            className="hamburger-svg"
-            alt="menu"
-        ></img>
+
+            <img
+                ref={hamburgerButton}
+                onClick={handleHamburgerClick}
+                src="/resources/hamburgerGray.png"
+                className="hamburger-svg"
+                alt="menu"
+            ></img>
         </div>
     );
 }
